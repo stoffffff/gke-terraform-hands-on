2a47c1755774de6334e67619155a2e5a14cc15f2
unknown <nafi.professional@outlook.com>
2025-07-08 16:05:19 +0100

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "gke_cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+  default     = "cool-gke-cluster"
+}
+
+variable "network_name" {
+  description = "VPC network name"
+  type        = string
+  default     = "flask-app-network"
+}
