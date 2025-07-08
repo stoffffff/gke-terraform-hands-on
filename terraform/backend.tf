266836b65ec650e3a04c62308e325591978b9b04
stoffffff <nafi.professional@outlook.com>
2025-07-08 15:13:26 +0000

    change state file bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1"
 
   backend "gcs" {
-    bucket  = "cybersapient-terraform-state-bucket"
-    prefix  = "gke-flask-app/terraform/state"
+    bucket  = "gke-terraform-state-bucket-demo"
+    prefix  = "gke-demo/terraform/state"
   }
 }
