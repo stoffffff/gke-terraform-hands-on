2745eac7740023c3a40c0d9197b79f76093632a9
stoffffff <nafi.professional@outlook.com>
2025-07-09 11:56:07 +0000

    push infra and pipeline code 'secrets updated'

---
+++ terraform/gke.tf
@@ -0,0 +1,27 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  networking_mode = "VPC_NATIVE"
+
+  ip_allocation_policy {}
+
+  release_channel {
+    channel = "REGULAR"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {}
+variable "region" {
+  default = "europe-west1"
+}
+variable "zone" {
+  default = "us-central1-b"
+}
+variable "cluster_name" {
+  default = "cool-k8s-cluster"
+}
