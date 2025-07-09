876cc54de02a28115c614443210fd69a762315fa
stoffffff <nafi.professional@outlook.com>
2025-07-09 11:33:43 +0000

    push react sample app files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>🚀 Frontend Deployed on GKE!</h1>
+      <p>This is a simple React app deployed using Docker, GitHub Actions, Helm, and GKE.</p>
+    </div>
+  )
+}
+
+export default App
--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.1"
-
-  backend "gcs" {
-    bucket  = "gke-terraform-state-bucket-demo"
-    prefix  = "gke-demo/terraform/state"
-  }
-}
